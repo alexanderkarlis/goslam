42543483c969baef00e7ee6fd73f97ef00963502
Alexander Karlis <alexanderkarlis@gmail.com>
2020-08-12 00:13:39 -0400

    new orbs

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # Go SLAM (Simultaneous localization and mapping)
 SLAM basics done here in Go, using OpenCV
 
+~~ Glorified Video player ~~
+~~ Successfully implmented basic orbs ~~
 ### Requirements
 - Go 1.14
 - OpenCV 4.x
 
 ### Run program
 ```sh
-> go run main.go output.mp4
+> go run main.go output.mp4 1000
 ```
 
 ### TODOs
 - ~~ Render video using opencv ~~
 - implement ORB
+    -- implemented some orbs (need more goodfeaturestotrack) 
 - figure out the goodFeaturesToTrack() for corner updating
 - MORE
--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"os"
+	"reflect"
+	"strconv"
 	"time"
 
 	"gocv.io/x/gocv"
@@ -11,23 +14,37 @@ import (
 var (
 	window = gocv.NewWindow("goslam")
 	orb    = gocv.NewORB()
+	bf     = gocv.NewBFMatcherWithParams(gocv.NormHamming, false)
+	last   = lastValues{}
+	feats  = gocv.NewMat()
 )
+
+type lastValues struct {
+	kps []gocv.KeyPoint
+	des gocv.Mat
+}
+
+type keypoints struct {
+	kp1 gocv.KeyPoint
+	kp2 gocv.KeyPoint
+}
 
 func main() {
 	defer window.Close()
 
-	if len(os.Args) < 2 {
-		fmt.Println("How to run:\n\tshowimage [imgfile]")
+	if len(os.Args) < 3 {
+		fmt.Println("How to run:\n\tgoslam [imgfile]")
 		return
 	}
 
 	filename := os.Args[1]
-
+	framerateMs, err := strconv.Atoi(os.Args[2])
+	checkErr(err)
 	frames, err := gocv.VideoCaptureFile(filename)
 	defer frames.Close()
 	checkErr(err)
 
-	ticker := time.NewTicker(100 * time.Millisecond)
+	ticker := time.NewTicker(time.Duration(framerateMs) * time.Millisecond)
 	done := make(chan bool)
 
 	for {
@@ -41,20 +58,48 @@ func main() {
 	}
 }
 
+func (x lastValues) IsStructureEmpty() bool {
+	return reflect.DeepEqual(x, lastValues{})
+}
+
 func getMatType(img *gocv.Mat) gocv.MatType {
 	fmt.Printf("Mat type: %+v\n", img.Type())
 	return img.Type()
 }
 
-func extract(img gocv.Mat) {
-	// var corners *gocv.Mat
-	corners := gocv.NewMat()
-	grey := gocv.NewMat()
-	gocv.CvtColor(img, &grey, gocv.ColorBGRToGray)
-	fmt.Printf("%v\n", grey.Rows())
+func extract(img *gocv.Mat) (ret []keypoints) {
+	// gocv.Resize(*img, img, image.Point{}, 400, 600, 0)
+	// detection
+	fmt.Printf("SIZE: %+v\n", img.Size())
+	cv8uci := gocv.NewMatWithSize(2160, 3840, gocv.MatTypeCV8UC1)
+	gocv.GoodFeaturesToTrack(cv8uci, &feats, 3000, 0.01, 3.0)
+	fmt.Printf("%+v\n", feats.Mean())
 
-	gocv.GoodFeaturesToTrack(grey, &corners, 3000, 0.01, 3)
-	fmt.Println(corners)
+	// extraction
+	corners := gocv.NewMat()
+	kps, _ := orb.DetectAndCompute(*img, corners)
+	gocv.DrawKeyPoints(*img, kps, img, color.RGBA{0, 0, 255, 0}, 0)
+	window.IMShow(*img)
+
+	// matching
+	// if !last.IsStructureEmpty() {
+	// 	matches := bf.KnnMatch(des, last.des, 2)
+	// 	for i, m := range matches {
+	// 		for _, n := range m {
+	// 			if m[i].Distance < 0.75*n.Distance {
+	// 				kp1 := kps[m[i].QueryIdx]
+	// 				kp2 := last.kps[m[i].TrainIdx]
+	// 				ret = append(ret, keypoints{
+	// 					kp1,
+	// 					kp2,
+	// 				})
+	// 			}
+
+	// 		}
+	// 	}
+	// }
+	// last = lastValues{kps: kps, des: des}
+	return
 }
 
 func processFrame(frames *gocv.VideoCapture) {
@@ -63,14 +108,10 @@ func processFrame(frames *gocv.VideoCapture) {
 	if !ok || img.Empty() {
 		return
 	}
-	extract(img)
-
-	// var greyImg gocv.Mat
-	// gocv.CvtColor(img, &greyImg, gocv.ColorBGRToGray)
-	// gocv.GoodFeaturesToTrack(img, &greyImg, 3000, 0.01, 3)
-
-	fmt.Printf("mean: %+v\n", img.Mean())
-	window.IMShow(img)
+	extract(&img)
+	// fmt.Printf("matches: %+v\n", matches)
+	// fmt.Printf("last: %+v\n", last)
+	// window.IMShow(img)
 	if window.WaitKey(1) >= 0 {
 		print("exiting display")
 		os.Exit(0)
